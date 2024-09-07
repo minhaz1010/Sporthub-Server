35c43f3193d433b32cb99c8a80e34f9a5784aa20
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-07 19:34:24 +0600

    add product schema

---
+++ src/app/module/product/product.schema.ts
@@ -0,0 +1,55 @@
+import { Schema, model } from 'mongoose';
+import { IProduct } from './product.interface';
+
+const ProductSchema = new Schema<IProduct>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  brand: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5,
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug:{
+    type:String
+  }
+}, {
+  timestamps: true, 
+});
+
+export const Product = model<IProduct>('Product', ProductSchema);
+
+
