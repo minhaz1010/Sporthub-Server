9eac3a49acc93f0f1aa88fd023817ff36554cd85
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-07 19:33:53 +0600

    add product interface

---
+++ src/app/module/product/product.interface.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+  name:string;
+  category:string;
+  stock:number;
+  brand:string;
+  description:string;
+  price:number;
+  rating:number;
+  image:string;
+  slug:string;
+}
